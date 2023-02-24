594f198395eddc3449486387351d58eef58260f7
omda3310 <6197@holbertonstudents.com>
2023-02-24 11:18:28 +0100

    test60

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -12,10 +12,10 @@ long int longintType;
 double longlongType;
 char charType;
 
-printf("Size of char: %ld byte\(s\)\n", sizeof(charType));
-printf("Size of int: %ld byte\(s\)\n", sizeof(integerType));
-printf("Size of long int: %ld byte\(s\)\n", sizeof(longintType));
-printf("Size of long long int: %ld byte\(s\)\n", sizeof(longlongType));
-printf("Size of float: %ld byte\(s\)\n", sizeof(floatType));
+printf("Size of a char: %ld byte\(s\)\n", sizeof(charType));
+printf("Size of an int: %ld byte\(s\)\n", sizeof(integerType));
+printf("Size of a long int: %ld byte\(s\)\n", sizeof(longintType));
+printf("Size of a long long int: %ld byte\(s\)\n", sizeof(longlongType));
+printf("Size of a float: %ld byte\(s\)\n", sizeof(floatType));
 return (0);
 }
