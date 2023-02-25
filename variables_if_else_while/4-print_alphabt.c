2ee10021b92516d286eef8e176d918b1b015699a
omda3310 <6197@holbertonstudents.com>
2023-02-25 23:19:39 +0100

    test4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main -  description main
+*Return: result
+*/
+int main(void)
+{
+char alphabet = 'a';
+while (alphabet <= 'z')
+{
+putchar(alphabet);
+alphabet++;
+}
+putchar('\n');
+return (0);
+}
