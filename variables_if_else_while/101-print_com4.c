e28d852cc94c446e460d9987d1743634cf39f83f
omda3310 <6197@holbertonstudents.com>
2023-02-26 09:54:05 +0100

    test101

---
+++ variables_if_else_while/101-print_com4.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/**
+*main -  descriptions
+*Return: responses
+*/
+int main(void)
+{
+int i;
+int j;
+int k;
+for (i = '0'; i <= '9'; ++i)
+{
+for (j = '1'; j <= '9'; ++j)
+{
+for (k = '3'; k <= '9'; k++)
+putchar(i);
+putchar(j);
+putchar(k);
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
