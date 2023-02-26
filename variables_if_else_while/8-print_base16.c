7083391eadf258c7d11e1c98d543989560c5d4c5
omda3310 <6197@holbertonstudents.com>
2023-02-26 09:01:40 +0100

    test8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+*main -  description
+*Return: responses
+*/
+int main(void)
+{
+int i;
+char ch;
+for (i = 0; i < 10; i++)
+{
+putchar(i);
+}
+for (ch = 'a'; ch <= 'f'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
