757e4ab1b26f0dd8ef538da954ee05a7f58238e1
omda3310 <6197@holbertonstudents.com>
2023-02-27 10:25:25 +0100

    test1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - check the code
+ * print_alphabet -  display alphabet
+ * @parameterx: void
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+char = alphabet;
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+putchar(alphabet);
+}
+putchar('\n');
+return (0);
+}
+int main(void)
+{
+print_alphabet();
+return (0);
+}
