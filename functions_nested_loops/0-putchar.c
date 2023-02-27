9af5acd15bc08a94efe1d67866201b6844a5b653
omda3310 <6197@holbertonstudents.com>
2023-02-27 11:22:15 +0100

    test0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -5,6 +5,13 @@
 */
 int main(void)
 {
-printf("\_putchar");
-return (1);
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
