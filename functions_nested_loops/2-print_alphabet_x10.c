f09a246e6d6ce527287bf896abf216e5908eadca
omda3310 <6197@holbertonstudents.com>
2023-02-28 22:55:24 +0100

    fixed

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - display 10 times
+ *
+ * parameterx: integer
+ */
+void print_alphabet_x10(void)
+{
+int i;
+char alphabet;
+for (i = 0; i < 10; i++)
+{
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+_putchar(alphabet);
+}
+_putchar('\n');
+}
+}
