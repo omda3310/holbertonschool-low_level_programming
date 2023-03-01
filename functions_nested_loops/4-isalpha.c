eed35064c0d6e149f856e763455c0c017cd68290
omda3310 <6197@holbertonstudents.com>
2023-03-01 10:59:19 +0100

    fix

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
-* _isalpha - display caracters
-* @c: variable
-*
-* Return: integer
-*/
+ * _isalpha - display caracters
+ * @c: variable
+ *
+ * Return: integer
+ */
 int _isalpha(int c)
 {
-if ((c >= 'a' && c <= 'z') || (c >= 'A' && c >= 'Z'))
-{
-return (1);
-}
-else
-{
-return (0);
-}
+	if (((c) >= 'A' && (c) <= 'Z') || ((c) >= 'a' && (c) <= 'z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
