28dd53a07d7537a223564deeb7ad0997ea05d924
omda3310 <6197@holbertonstudents.com>
2023-03-02 09:51:42 +0100

    add

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,19 +1,16 @@
 #include "main.h"
 /**
-* _abs - absolute value
-* @n: integer
-* Return: integer
-*/
+ * _abs - absolute value
+ * @n: integer
+ * Return: integer
+ */
 int _abs(int n)
 {
 
-
-if (n >= 0)
-{
-return (n);
-}
-
-n = -n;
-return (n);
-
+	if (n >= 0)
+	{
+		return (n);
+	}
+	n = -n;
+	return (n);
 }
