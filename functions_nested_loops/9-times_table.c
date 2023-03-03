4617f4f276126f2c6ca3bf739debfe87eb2fd890
omda3310 <6197@holbertonstudents.com>
2023-03-03 10:49:47 +0100

    fixed

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * times_tables - 9 times
+ *
+ * Return: None
+ */
+void times_table(void)
+{
+	int i, j, mult;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			mult = i * j;
+
+			if (mult <= 9)
+			{
+				_putchar(mult + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(' ');
+			}
+			else
+			{
+				int d = mult / 10;
+				int r = mult % 10;
+
+				_putchar(d + '0');
+				_putchar(r + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
