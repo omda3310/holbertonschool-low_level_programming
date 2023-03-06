c4c84ab79af553c4dac48945774ecbcb83e5f1cb
omda3310 <6197@holbertonstudents.com>
2023-03-06 11:05:47 +0100

    add 3-puts.c

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _puts - display string
+ * @str: variable
+ *
+ */
+void _puts(char *str)
+{
+	int i;
+	int l = strlen(str) + 1;
+
+	for (i = 0; i < l; i++)
+	{
+		char ch = str[i];
+
+		if (ch == '\n')
+			break;
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
