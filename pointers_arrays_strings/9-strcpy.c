2b811a2d84e51e58b23a9412202ba051759daa8b
omda3310 <6197@holbertonstudents.com>
2023-03-08 12:31:32 +0100

    add 9-strcpy.c

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
- * *_strcpy - concat two strings
+ * _strcpy - concat two strings
  * @dest: variable1
  * @src: variable2
+ * Return: array of caracters
  */
 char *_strcpy(char *dest, char *src)
 {
