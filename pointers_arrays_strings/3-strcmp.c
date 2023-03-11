883c89883bed25647051ca723575fff279ee16df
omda3310 <6197@holbertonstudents.com>
2023-03-11 10:03:22 +0100

    add 3-strcmp.c

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -9,14 +9,12 @@ int _strcmp(char *s1, char *s2)
 {
 	while (*s1 != '\0')
 	{
-		if (*s2 == '\0')
-			return (15);
 		if (*s1 > *s2)
 			return (15);
 		if (*s1 < *s2)
 			return (-15);
-		++s1;
-		++s2;
+		s1++;
+		s2++;
 	}
 	if (*s2 != '\0')
 		return (-15);
