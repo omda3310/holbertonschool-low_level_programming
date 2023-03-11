306c2ebe1a5f45c49f13fe41749eaf87ecd677e9
omda3310 <6197@holbertonstudents.com>
2023-03-11 09:25:54 +0100

    add 3strcmp.c

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strcmp - compare two strings
+ * @s1: string 1
+ * @s2: etring 2
+ * Return: int
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 != '\0')
+	{
+		if (*s2 == '\0')
+			return (15);
+		if (*s1 > *s2)
+			return (15);
+		if (*s1 < *s2)
+			return (-15);
+		s1++;
+		s2++;
+	}
+	if (*s2 != '\0')
+		return (-15);
+	return (0);
+}
