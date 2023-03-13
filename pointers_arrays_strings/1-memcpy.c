339ed8db24d71be07d3b1f1c8698b898d6a3564d
omda3310 <6197@holbertonstudents.com>
2023-03-13 10:54:17 +0100

    add 1-memcpy.c

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -16,11 +16,8 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 		{
 			dest[i] = src[i];
 			i++;
-		}
-		if (src[i] == '\0' && i < n)
-		{
-			dest[i] = '\0';
-			i++;
+			if (src[i] == '\0' && i < n)
+				dest[i] = '\0';
 		}
 	}
 	return (dest);
