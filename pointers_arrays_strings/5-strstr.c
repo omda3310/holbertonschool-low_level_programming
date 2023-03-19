8d1729c5a19a896e8c00cd0ce89e02b1bb0f952b
omda3310 <6197@holbertonstudents.com>
2023-03-19 22:55:07 +0100

    add 5-strstr.c

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strstr - display length of occur
+ * @s: string
+ * @accept: string 2
+ * Return: integer
+ */
+char *_strstr(char *s, char *accept)
+{
+	int i, n;
+	char *pt;
+
+	for (i = 0, n = 0; accept[i] != '\0'; i++)
+	{
+		if (s[i] == accept[n])
+		{
+			if (n == 0)
+				pt = &s[i];
+			if (accept[n + 1] == '\0')
+				return (pt);
+			++n;
+		}
+		else
+			n = 0;
+	}
+	return (0);
+}
