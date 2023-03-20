06dec894bf86934febf9f162d845c3e5afeca911
omda3310 <6197@holbertonstudents.com>
2023-03-20 21:48:00 +0100

    add 8-print_diagsums.c

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -10,8 +10,8 @@ void print_diagsums(int *a, int size)
 {
 	int i = 0;
 	int j = size - 1;
-	int sum1 = 0;
-	int sum2 = 0;
+	unsigned int sum1 = 0;
+	unsigned int sum2 = 0;
 
 	for (i = 0; i <= size * size; i = i + size + 1)
 	{
