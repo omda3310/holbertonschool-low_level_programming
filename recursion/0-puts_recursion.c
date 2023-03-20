deafb13d8a8b563e58b0efcacef7e9fd709672c5
omda3310 <6197@holbertonstudents.com>
2023-03-20 23:02:29 +0100

    add 0-puts-recursion

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -3,14 +3,14 @@
  * _puts_recursion -  function that prints a string
  * @s: string
  *
- **
+ *
  */
 void _puts_recursion(char *s)
 {
-    if (*s != '\0')
-    {
-        _putchar(*s);
-        _puts_recursion(s + 1);
-    }
-    _putchar('\n');
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	_putchar('\n');
 }
