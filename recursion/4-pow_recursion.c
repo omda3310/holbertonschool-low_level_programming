1cd40e5820194d9d9cc4dc6640ad0e198f8fdb37
omda3310 <6197@holbertonstudents.com>
2023-03-26 15:35:03 +0200

    add 4-pow_recursion.c

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _pow_recursion - recursion
+ * @x: variable 1
+ * @y: variable 2
+ * Return: integer
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
