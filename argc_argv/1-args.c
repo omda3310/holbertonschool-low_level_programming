c8700960654bc8cedc979af2689014671a73779e
omda3310 <6197@holbertonstudents.com>
2023-03-26 18:41:36 +0200

    add 1-args.c

---
+++ argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print number of arguments
+ * argc: number of argument
+ * argv: pointer
+ * Return: return 0
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	while (i != argc - 1)
+	{
+		i++;
+		argv++;
+	}
+
+	printf("%d\n", i);
+	return (0);
+}
