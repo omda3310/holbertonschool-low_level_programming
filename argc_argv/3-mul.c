ba32386d5347a9cf5c38e6de959289ae960d4535
omda3310 <6197@holbertonstudents.com>
2023-03-26 19:16:03 +0200

    add 3-mul.c

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print number of arguments
+ * @argc: number of argument
+ * @argv: pointer
+ * Return: return 0
+ */
+int main(int argc, char *argv[])
+{
+	int prod;
+
+	if (argc < 2)
+	{
+		printf("Erreur");
+		return (1);
+	}
+	else
+	{
+		argc = 1;
+		prod = atoi(argv[argc]) * atoi(argv[argc + 1]);
+		printf("%d\n", prod);
+		return (0);
+	}
+}
