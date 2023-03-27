f62680e29522cd03e67bbfe4e720bb7c25b8de83
omda3310 <6197@holbertonstudents.com>
2023-03-27 02:19:47 +0200

    add 1-strdup.c

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - duplicate of string
+ * @str: string
+ * Return: duplicate of string
+ *
+ */
+char *_strdup(char *str)
+{
+	int i = 0, j = 0;
+	char *p;
+
+	for (i = 0; str[i]; i++)
+		;
+	p = malloc(sizeof(char) * i);
+	if (i == 0 || p == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		while (j < i)
+		{
+			p[j] = str[j];
+			j++;
+		}
+		return (p);
+	}
+}
