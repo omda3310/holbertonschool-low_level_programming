ae572412093a5e67a9bceca62b315ea5e0904905
omda3310 <6197@holbertonstudents.com>
2023-03-27 02:01:42 +0200

    add 0-create_array.c

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -8,21 +8,21 @@
  */
 char *create_array(unsigned int size, char c)
 {
-    char *p;
-    unsigned int i = 0;
+	char *p;
+	unsigned int i = 0;
 
-    p = malloc(sizeof(char) * size);
-    if (size == 0)
-    {
-        return (0);
-    }
-    else
-    {
-        while (i < size)
-        {
-            p[i] = c;
-            i++;
-        }
-    }
-    return (p);
+	p = malloc(sizeof(char) * size);
+	if (size == 0 || p == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		while (i < size)
+		{
+			p[i] = c;
+			i++;
+		}
+	}
+	return (p);
 }
