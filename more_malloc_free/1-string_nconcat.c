d2440100c6fb3960569973995c2e785b0975d08d
omda3310 <6197@holbertonstudents.com>
2023-03-30 01:40:30 +0200

    add 1-string_nconcat.c

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -9,10 +9,9 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int l, l1, l2, i, j = 0;
+	unsigned int l1 = 0, l2 = 0, i, j;
 	char *p;
 
-	l = n;
 	if (s1 == NULL)
 	{
 		s1 = "";
@@ -21,17 +20,17 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		s2 = "";
 	}
-	for (i = 0; s1; i++)
+	for (i = 0; s1[i]; i++)
 	{
 		l1++;
 	}
-	for (j = 0; s2; j++)
+	for (j = 0; s2[j]; j++)
 	{
 		l2++;
 	}
-	if (l >= l2)
+	if (n >= l2)
 	{
-		l = l2;
+		n = l2;
 	}
 	p = malloc((l1 + n + 1) * sizeof(char));
 	if (!p)
@@ -42,10 +41,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		p[i] = s1[i];
 	}
-	for (; i < l1 + l; i++)
+	for (j = 0; j < n; j++)
 	{
 		p[i] = s2[j];
-		j++;
+		i++;
 	}
 	p[i] = '\0';
 	return (p);
