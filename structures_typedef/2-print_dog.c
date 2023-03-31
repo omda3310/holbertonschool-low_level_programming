3066574dc311a69cfc2d7077b8165149110994e0
omda3310 <6197@holbertonstudents.com>
2023-04-01 01:43:31 +0200

    add 2-print_dog.c

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -20,7 +20,14 @@ void print_dog(struct dog *d)
 	{
 		printf("Name: %s\n", d->name);
 	}
-	printf("Age: %f\n", d->age);
+	if (d->age == NULL)
+	{
+		printf("Age: nil\n");
+	}
+	else
+	{
+		printf("Age: %f\n", d->age);
+	}
 	if (d->owner == NULL)
 	{
 		printf("Owner: nil\n");
