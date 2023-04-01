8badc1a5e63b2438c0a5572ae0da09391b8ff08c
omda3310 <6197@holbertonstudents.com>
2023-04-02 01:10:16 +0200

    add 4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -13,7 +13,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *d;
 	int i, j;
 
-	d = malloc(sizeof(dog_t));
+	d = malloc(sizeof(*new_dog));
 	if (d == NULL || !(name) || !(owner))
 	{
 		free(d);
