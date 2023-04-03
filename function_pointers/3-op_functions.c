1ba6be0d8c85f04815e27516a8a8d76855b0f659
omda3310 <6197@holbertonstudents.com>
2023-04-03 17:17:27 +0200

    add 3-op_functions.c

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ * op_add - sum of two integers
+ * @a: variable1
+ * @b: variable2
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - substraction
+ * @a: variable1
+ * @b: variable2
+ * Return: substraction
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_sub - multiplication
+ * @a: variable1
+ * @b: variable2
+ * Return: multiplication
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_sub - division
+ * @a: variable1
+ * @b: variable2
+ * Return: division
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_sub - a % b
+ * @a: variable1
+ * @b: variable2
+ * Return: a % b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
