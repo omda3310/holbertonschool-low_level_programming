9e9eb875a500773f5938d87d07c4516bbfd7d6e8
omda3310 <6197@holbertonstudents.com>
2023-04-03 14:54:33 +0200

    add 2-int_index.c

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+ * int_index -  searches for an integer
+ * @array: array of integer
+ * @size: size of zrray
+ * @cpm: function to compare two integers
+ * Return: index
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || size <= 0 || cmp == NULL)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
