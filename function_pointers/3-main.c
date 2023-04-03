280ad1d992b8eba3315691be8a4dfef995e2c60c
omda3310 <6197@holbertonstudents.com>
2023-04-03 17:26:16 +0200

    add 3-main.c

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -9,28 +9,28 @@
  */
 int main(int argc, char *argv[])
 {
-    char *op;
-    int r, num1, num2;
+	char *op;
+	int r, num1, num2;
 
-    if (argc != 4)
-    {
-        printf("Error\n");
-        exit(98);
-    }
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[3]);
-    op = argv[2];
-    if ((op[0] == '/' && num2 == 0) || (op[0] == '%' && num2 == 0))
-    {
-        printf("Error\n");
-        exit(100);
-    }
-    if (op[1] != '\0' || !num1 || !num2 || !get_op_func(op))
-    {
-        printf("Error\n");
-        exit(99);
-    }
-    r = get_op_func(op)(num1, num2);
-    printf("%d\n", r);
-    return (0);
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	op = argv[2];
+	if ((op[0] == '/' && num2 == 0) || (op[0] == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	if (op[1] != '\0' || !num1 || !num2 || !get_op_func(op))
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	r = get_op_func(op)(num1, num2);
+	printf("%d\n", r);
+	return (0);
 }
