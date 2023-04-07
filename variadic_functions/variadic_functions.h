5b4bed2b26cf329df42db6ef30ec54a69b46322d
omda3310 <6197@holbertonstudents.com>
2023-04-07 10:34:25 +0200

    update

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -6,4 +6,16 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char *const format, ...);
 
+#include <stdarg.h>
+/**
+ * struct print - data struct
+ * @c:char
+ * @print_func: function pointers
+ */
+typedef struct print
+{
+    char *c;
+    void (*print_func)(va_list vl);
+} printd;
+
 #endif
