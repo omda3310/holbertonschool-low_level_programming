13fa6176b0c4e92645c94beb431bce3c7a134ba6
omda3310 <6197@holbertonstudents.com>
2023-04-11 02:02:38 +0200

    add 4-free_list.c

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_list - free list
+ * @head: list
+ *
+ *
+ */
+void free_list(list_t *head)
+{
+    list_t *l;
+
+    while (head)
+    {
+        l = head;
+        head = head->next;
+        free(l->str);
+        free(l);
+    }
+    free(head);
+}
