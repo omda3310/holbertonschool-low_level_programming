25f3a4c0369013980737fb5a029ae39cb940fa05
omda3310 <6197@holbertonstudents.com>
2023-04-30 11:37:43 +0200

    add 5-get_dnodeint.c

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -9,18 +9,18 @@
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-    unsigned int i = 0;
-    dlistint_t *current = head;
+	unsigned int i = 0;
+	dlistint_t *current = head;
 
-    while (current != NULL && i < index)
-    {
-        current = current->next;
-        i++;
-    }
-    if (i != index || current == NULL)
-    {
-        return (NULL);
-    }
+	while (current != NULL && i < index)
+	{
+		current = current->next;
+		i++;
+	}
+	if (i != index || current == NULL)
+	{
+		return (NULL);
+	}
 
-    return (current);
+	return (current);
 }
