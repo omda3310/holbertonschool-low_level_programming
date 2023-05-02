03fcacf7fbd55e6e22774a06f8db66d8e2e23645
omda3310 <6197@holbertonstudents.com>
2023-05-02 13:29:39 +0200

    add struct

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -1,1 +1,30 @@
+#ifndef HASH_TABLE_H
+#define HASH_TABLE_H
+
+/**
+ * struct hash_node - Node of a hash table
+ *
+ * @key: The key, string
+ * @value: The value corresponding to a key
+ * @next: A pointer to the next node of the List
+ */
+typedef struct hash_node_s
+{
+    char *key;
+    char *value;
+    struct hash_node_s *next;
+} hash_node_s;
+/**
+ * struct hash_table - Hash table data structure
+ *
+ * @size: The size of the array
+ * @array: An array of size @size
+ */
+typedef struct hash_table_t
+{
+    unsigned long int size;
+    hash_node_s **array;
+} hash_table_t;
 hash_table_t *hash_table_create(unsigned long int size);
+
+#endif
