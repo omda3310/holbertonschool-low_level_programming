34f5f411e4ee53d1b2e45baff2aeddb428843b41
omda3310 <6197@holbertonstudents.com>
2023-05-02 16:07:55 +0200

    add 0-isupper.c

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _isupper - check the code.
+ *
+ * @c : check the code.
+ *
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
