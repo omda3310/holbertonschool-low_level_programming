e4b998a657e91d8f2c3d299da0333ce27b69406f
omda3310 <6197@holbertonstudents.com>
2023-05-02 14:45:40 +0200

    update

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -1,53 +1,30 @@
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 #include "hash_tables.h"
-#include "string.h"
 /**
- *hash_table_set - A function that sets a key value pair in the hash table.
- * @ht: is the hash table you want to add or update the key/value to
- * @key: is the key
- * @value: is the value associated with the key
- * Return: Always 0.
+ * hash_table_get - check the code
+ * @ht: is the hash table you want to look into
+ * @key: is the key you are looking for
+ * Return: Always EXIT_SUCCESS.
  */
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int index = 0;
+	unsigned long int index;
+	hash_node_t *temp_node;
 
-	hash_node_t *new_node = NULL, *temp_node = NULL;
+	if (!ht || !key || !*key)
+		return (NULL);
 
-	if (!ht || !key || strlen(key) == 0)
-		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 	temp_node = ht->array[index];
-	while (temp_node != NULL)
+
+	while (temp_node)
 	{
 		if (strcmp(temp_node->key, key) == 0)
-		{
-			free(temp_node->value);
-			temp_node->value = strdup(value);
-			if (!temp_node->value)
-				return (0);
-			return (1);
-		}
+			return (temp_node->value);
 		temp_node = temp_node->next;
 	}
-	new_node = malloc(sizeof(hash_node_t));
-	if (!new_node)
-		return (0);
-	new_node->key = strdup(key);
-	if (!new_node->key)
-	{
-		free(new_node);
-		return (0);
-	}
-	new_node->value = strdup(value);
-	if (!new_node->value)
-	{
-		free(new_node->key);
-		free(new_node);
-		return (0);
-	}
-	new_node->next = ht->array[index];
-	ht->array[index] = new_node;
-	return (1);
+
+	return (NULL);
 }
