a027f918638b24b6147a9448cff0a420d3a6f033
omda3310 <6197@holbertonstudents.com>
2023-05-28 11:58:23 +0200

    linear search

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -3,7 +3,7 @@
 #include "search_algos.h"
 
 /**
- * binary_search - searches for a value in an array of integers
+ * linear_search - searches for a value in an array of integers
  *                 using the Linear search algorithm
  * @array: array of integers
  * @size: size of array
