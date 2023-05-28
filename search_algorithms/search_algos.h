f8534dedec13f2a6f2b54a401bc9fa0a600f0875
omda3310 <6197@holbertonstudents.com>
2023-05-28 11:50:05 +0200

    Header file

---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,9 @@
+#ifndef SEARCH_HEADER
+#define SEARCH_HEADER
+#include <stdio.h>
+#include <stdlib.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
