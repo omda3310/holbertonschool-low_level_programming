241894ba7e03365cad42119c58d867244803a8f5
omda3310 <6197@holbertonstudents.com>
2023-05-28 11:51:05 +0200

    linear search

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+
+/**
+ * binary_search - searches for a value in an array of integers
+ *                 using the Linear search algorithm
+ * @array: array of integers
+ * @size: size of array
+ * @value: value to search
+ * Return: integer
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
